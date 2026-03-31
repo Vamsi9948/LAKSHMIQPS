cb94f0429951800014faeae1a7a2488e192785f7
Vamsi9948 <100012523+Vamsi9948@users.noreply.github.com>
2026-03-31 10:55:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ except Exception as e:
     st.stop()
 
 # --- 2. SESSION STATE & COOKIE MANAGER ---
-@st.cache_resource(experimental_allow_widgets=True)
+@st.cache_resource
 def get_cookie_manager():
     return stx.CookieManager()
 
